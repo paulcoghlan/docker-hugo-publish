181ff42d06309c6ca81795f28bb7517b634f63ac
Paul Coghlan <git@paulcoghlan.com>
2025-02-10 20:39:28 +0000

    Really add docker compose

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Server command 
-hugo server -D --bind 0.0.0.0 --source=/source --renderStaticToDisk --baseURL=$BASE_URL --appendPort=false
+# hugo server -D --bind 0.0.0.0 --source=/source --renderStaticToDisk --baseURL=$BASE_URL --appendPort=false
+#
+# Build command
+#
+hugo --source=/source
